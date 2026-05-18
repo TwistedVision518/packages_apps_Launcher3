a90fd9ae0713ce6f7524626e4b88c5116bd84561
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-18 21:53:02 +0800

    Launcher3: Fixing wp depth animation issues
    Change-Id: Ic9b70f71494933d12caf7098bcf91f8c44040ba9
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -323,7 +323,15 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
 
                 @Override
                 public void playUnlockAnimation(boolean unlocked, long duration, long startDelay) {
-                    MAIN_EXECUTOR.execute(() -> playWorkspaceUnlockReveal(unlocked, startDelay));
+                    MAIN_EXECUTOR.execute(() -> playWorkspaceUnlockReveal(unlocked, startDelay,
+                            false));
+                }
+
+                @Override
+                public void playUnlockAnimationWithWallpaperDepth(boolean unlocked, long duration,
+                        long startDelay, boolean animateWallpaperDepth) {
+                    MAIN_EXECUTOR.execute(() -> playWorkspaceUnlockReveal(unlocked, startDelay,
+                            animateWallpaperDepth));
                 }
 
                 @Override
@@ -2039,7 +2047,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
         setWorkspaceUnlockRevealAmount(0f);
     }
 
-    private void playWorkspaceUnlockReveal(boolean unlocked, long startDelay) {
+    private void playWorkspaceUnlockReveal(boolean unlocked, long startDelay, boolean animateDepth) {
         if (!unlocked) {
             prepareWorkspaceUnlockReveal();
             return;
@@ -2047,7 +2055,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
 
         setWorkspaceUnlockRevealAmount(0f);
         mKeyguardUnlockAnimator = new WorkspaceRevealAnim(mLauncher, false /* animateOverviewScrim */,
-                false /* animateDepth */).getAnimators();
+                animateDepth).getAnimators();
         mKeyguardUnlockAnimator.setStartDelay(startDelay);
         mKeyguardUnlockAnimator.addListener(new AnimatorListenerAdapter() {
             @Override
